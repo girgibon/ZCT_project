770fa8d9d09ea9f01e8f56bcac6552d9bc216d5c
girgibon <93397589+girgibon@users.noreply.github.com>
2024-04-24 23:26:59 +0200

    Delete frontend directory

--- frontend/src/models/IEvent.ts
+++
@@ -1,6 +0,0 @@
-export interface IEvent {
-    title: string;
-    date: string;
-    description: string;
-    author: string;
-}
--- frontend/src/store/reducers/action-creators.ts
+++
@@ -1,7 +0,0 @@
-import { AuthActionCreators } from "./auth/action-creators"
-import { EventActionCreators } from "./event/action_creators"
-
-export const allActionCreators = {
-    ...AuthActionCreators,
-    ...EventActionCreators
-}
